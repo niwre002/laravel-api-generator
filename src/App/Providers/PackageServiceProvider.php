54e32f751328f32263e907ec3637875095c260a2
Erwin <erwin@fastfwd.com>
2023-02-12 13:06:05 +0000

    #patch: Updated src

---
+++ src/App/Providers/PackageServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Erwinnerwin\LaravelApiGenerator;
+ 
+use Illuminate\Support\ServiceProvider;
+
+class PackageServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+
+    }
+ 
+    public function register()
+    {
+     
+    }
+}
